c7fbabc8cb5a64e406930cb1b2d9db8a44ac769d
dina-usama <39261594+AhmedAbouzaid1@users.noreply.github.com>
2021-04-15 04:23:52 +0200

    BiGRU instead of BiLSTM

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ labels = ["contradiction", "entailment", "neutral"]
 train_df = pd.read_csv("SNLI_Corpus/snli_1.0_train.csv", nrows=100000)
 valid_df = pd.read_csv("SNLI_Corpus/snli_1.0_dev.csv")
 test_df = pd.read_csv("SNLI_Corpus/snli_1.0_test.csv")
-# train_df = train_df[:10000]
-# valid_df = valid_df[:10000]
-# test_df = test_df[:1000]
+train_df = train_df[:10000]
+valid_df = valid_df[:10000]
+test_df = test_df[:1000]
 # Shape of the data
 print(f"Total train samples : {train_df.shape[0]}")
 print(f"Total validation samples: {valid_df.shape[0]}")
@@ -169,12 +169,12 @@ with strategy.scope():
         input_ids, attention_mask=attention_masks, token_type_ids=token_type_ids
     )
     # Add trainable layers on top of frozen layers to adapt the pretrained features on the new data.
-    bi_lstm = tf.keras.layers.Bidirectional(
-        tf.keras.layers.LSTM(64, return_sequences=True)
+    BiGRU = tf.keras.layers.Bidirectional(
+        tf.keras.layers.GRU(64, return_sequences=True)
     )(sequence_output)
     # Applying hybrid pooling approach to bi_lstm sequence output.
-    avg_pool = tf.keras.layers.GlobalAveragePooling1D()(bi_lstm)
-    max_pool = tf.keras.layers.GlobalMaxPooling1D()(bi_lstm)
+    avg_pool = tf.keras.layers.GlobalAveragePooling1D()(BiGRU)
+    max_pool = tf.keras.layers.GlobalMaxPooling1D()(BiGRU)
     concat = tf.keras.layers.concatenate([avg_pool, max_pool])
     dropout = tf.keras.layers.Dropout(0.3)(concat)
     output = tf.keras.layers.Dense(3, activation="softmax")(dropout)
